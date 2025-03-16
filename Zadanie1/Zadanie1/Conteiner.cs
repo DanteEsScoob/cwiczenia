eaded2214939e4976d7d6477a17b32b300c4b524
DanteEsScoobarre <116679105+DanteEsScoobarre@users.noreply.github.com>
2025-03-16 21:57:28 +0100

    Rozpoczęcie zadania

---
+++ Zadanie1/Zadanie1/Conteiner.cs
@@ -0,0 +1,10 @@
+﻿namespace Zadanie1;
+
+public class Conteiner
+{
+    private int masa;
+    private int wysokosc;
+    private int wagaWlasna;
+    private double glebokosc;
+    private string nr = "";
+}
