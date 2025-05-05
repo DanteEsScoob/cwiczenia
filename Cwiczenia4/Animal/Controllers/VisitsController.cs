6a42f37df58ae7432366471f29543e9534a943d8
DanteEsScoobarre <116679105+DanteEsScoobarre@users.noreply.github.com>
2025-05-05 12:10:08 +0200

    I hope it works

---
+++ Cwiczenia4/Animal/Controllers/VisitsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Animal.Models;
+
+namespace Animal.Controllers
+{
+    [Route("api/animal/{animalId}/visits")]
+    [ApiController]
+    public class VisitsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Visit>> GetAllVisits(int animalId)
+        {
+            var visits = Database.Visits
+                .Where(v => v.AnimalId == animalId)
+                .ToList();
+            return Ok(visits);
+        }
+
+        [HttpGet("{visitId}")]
+        public ActionResult<Visit> Get(int animalId,int visitId)
+        {
+            var visit = Database.Visits
+                .FirstOrDefault(v => v.AnimalId == animalId && v.VisitId == visitId);
+            
+            if (visit == null) return NotFound();
+            return Ok(visit);
+        }
+
+        [HttpPost]
+        public ActionResult<Visit> Add(int animalId, [FromBody] Visit visit)
+        {
+            visit.AnimalId = animalId;
+            visit.VisitDate = DateTime.Now;
+            
+            Database.Visits.Add(visit);
+            return CreatedAtAction(nameof(Get), new { animalId, visitId = visit.VisitId }, visit);
+        }
+        
+
+    }
+}
---
+++ Cwiczenia4/Animal/Models/Animal.cs
@@ -0,0 +1,29 @@
+﻿namespace Animal.Models;
+
+public class Animal
+{
+    private static int _id = 0;
+
+    public int Id { get; private set; }
+    public string Name{get;set;}
+    public string Species{get;set;}
+    public int Weight{get;set;}
+    public string Color{get;set;}
+
+
+    public Animal(string name, string species, int weight, string color)
+    {
+        Id = ++_id;
+        this.Name = name;
+        this.Species = species;
+        this.Weight = weight;
+
+        this.Color = color;
+    }
+
+    public Animal()
+    {
+        Id = ++_id;
+    }
+
+}
